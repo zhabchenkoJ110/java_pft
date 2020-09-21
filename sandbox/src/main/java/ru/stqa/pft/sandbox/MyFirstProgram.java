ccac262a5e19b239e975f28caf0506303722574f
Anastasiia <anastasiazh26@gmail.com>
2020-09-21 18:15:10 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,7 +4,30 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        hello("world");
+        hello("user");
+        hello("Anastasia");
+
+        double l = 5;
+        System.out.println("Площадь квадртата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+
 
     }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
+
 }
