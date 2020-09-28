1e8f02b78dc7ad9776f0d8d66447b129a84d61e6
Anastasiia <anastasiazh26@gmail.com>
2020-09-28 11:50:28 +0300

    Перенос части кода в метод инициализации фикстуры

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -12,10 +12,6 @@ public class GroupCreationTests {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
-
-    @Test
-    public void testGroupsCreation() throws Exception {
         wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -23,6 +19,10 @@ public class GroupCreationTests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.id("LoginForm")).submit();
+    }
+
+    @Test
+    public void testGroupCreation() throws Exception {
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
@@ -42,7 +42,6 @@ public class GroupCreationTests {
     @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
         wd.quit();
-
     }
 
     private boolean isElementPresent(By by) {
