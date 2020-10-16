0c9fecfc6feba788f32c602e23de7d0690982fd2
Anastasiia <anastasiazh26@gmail.com>
2020-10-16 22:35:12 +0300

    Лекция 5.2. Улучшение кода тестов: ещё немного реструктуризации кода

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook.tests;
 
-
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -9,6 +8,14 @@ import java.util.List;
 
 
 public class GroupDeletionTests extends TestBase {
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
+    }
 
     @Test
     public void testGroupDeletion() throws Exception {
