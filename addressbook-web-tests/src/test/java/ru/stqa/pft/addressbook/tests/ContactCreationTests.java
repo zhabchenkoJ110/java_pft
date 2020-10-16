3fefab174504bda24aa601df4c9a9819ae5855b9
Anastasiia <anastasiazh26@gmail.com>
2020-10-16 22:23:31 +0300

    Лекция 5.1. Как правильно запускать браузер

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class ContactCreationTests extends TestBase {
 
 
-    @Test
+    @Test (enabled = false)
     public void testContactCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         if (!app.getGroupHelper().isThereAGroup()) {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
-    @Test
+    @Test (enabled = false)
     public void testContactModification() {
         app.getNavigationHelper().gotoHomePage();
         if (!app.getContactHelper().isThereAContact()) {
