356c7f148d0b0cac3a90e7cde5b6dd129b1bbaa5
Anastasiia <anastasiazh26@gmail.com>
2020-10-25 21:49:52 +0300

    Лекция 6.4. Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -14,19 +14,19 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
-    @Parameter (names = "-c", description = "Group count")
+    @Parameter(names = "-c", description = "Group count")
     public int count;
 
-    @Parameter (names = "-f", description = "Target count")
+    @Parameter(names = "-f", description = "Target count")
     public String file;
 
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         GroupDataGenerator generator = new GroupDataGenerator();
         JCommander jCommander = new JCommander(generator);
         try {
             jCommander.parse(args);
-        } catch (ParameterException ex){
+        } catch (ParameterException ex) {
             jCommander.usage();
             return;
         }
@@ -42,8 +42,8 @@ public class GroupDataGenerator {
     private void save(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
-        for(GroupData group: groups){
-            writer.write(String.format("%s;%s;%s\n",group.getName(),group.getHeader(), group.getFooter()));
+        for (GroupData group : groups) {
+            writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
         }
         writer.close();
     }
@@ -52,7 +52,7 @@ public class GroupDataGenerator {
     //генератор тестовых данных
     private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
-        for (int i =0; i<count; i++){
+        for (int i = 0; i < count; i++) {
             groups.add(new GroupData().withName(String.format("test %s", i))
                     .withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
         }
