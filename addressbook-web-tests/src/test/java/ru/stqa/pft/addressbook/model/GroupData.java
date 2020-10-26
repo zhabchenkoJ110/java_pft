fb811ac9eaf560fd3b02d4ec5d529103b9991241
Anastasiia <anastasiazh26@gmail.com>
2020-10-26 19:25:59 +0300

    Лекция 6.7. Формат JSON

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.model;
 
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
@@ -8,8 +9,11 @@ import com.thoughtworks.xstream.annotations.XStreamOmitField;
 public class GroupData {
     @XStreamOmitField
     private int id = Integer.MAX_VALUE;
+    @Expose
     private String name;
+    @Expose
     private String header;
+    @Expose
     private String footer;
 
     public int getId() {
