c279f01472ffca2242e544abca0eb8b75c897279
Anastasiia <anastasiazh26@gmail.com>
2020-10-29 14:36:54 +0300

    Лекция 6.11. Протоколирование (лог-файлы) - правки
    Изменение уровня вывода лога ( с debug на info)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -17,7 +17,7 @@ public class TestBase {
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app
-            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
