36e6763eef9c441de9aabad49bbe4fe47e194659
Anastasiia <anastasiazh26@gmail.com>
2020-11-07 19:35:10 +0300

    Лекция 8.3. Прямое взаимодействие с сервером по протоколу HTTP - правка адреса запроса

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -26,7 +26,7 @@ public class HttpSession {
 
     //выполнение логин
     public boolean login(String username, String password) throws IOException {
-        HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login_page.php");
+        HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
         List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("username", username));
         params.add(new BasicNameValuePair("password", password));
