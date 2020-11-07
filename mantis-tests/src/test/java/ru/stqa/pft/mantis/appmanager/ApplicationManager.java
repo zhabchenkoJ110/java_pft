2c7b65f776cb945819fac2b423dd1088fec39ed0
Anastasiia <anastasiazh26@gmail.com>
2020-11-07 20:36:46 +0300

    Лекция 8.4. Ленивая инициализация помощников

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -14,8 +14,10 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     private final String browser;
+    private WebDriver wd;
+
     private final Properties properties;
-    WebDriver wd;
+    private RegistrationHelper registrationHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -25,22 +27,14 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-        if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
-        }
-        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS); //имплицитное ожидание
-        wd.get(properties.getProperty("web.baseUrl"));
     }
 
 
     public void stop() {
         //logout();
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
     }
 
 //    private void logout() {
@@ -53,5 +47,28 @@ public class ApplicationManager {
 
     public String getProperty(String key) {
         return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    //ленивая инициализация драйвера
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS); //имплицитное ожидание
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
     }
 }
