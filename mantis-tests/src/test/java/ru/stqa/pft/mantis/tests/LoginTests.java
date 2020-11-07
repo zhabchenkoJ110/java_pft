054b73bed041c4dac24ba5d68d15fc4f743ebc9c
Anastasiia <anastasiazh26@gmail.com>
2020-11-07 20:09:42 +0300

    Лекция 8.3. Прямое взаимодействие с сервером по протоколу HTTP - окончательные правки - часть2

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
@@ -12,7 +12,7 @@ public class LoginTests extends TestBase {
     @Test
     public void testLogin() throws IOException {
         HttpSession session = app.newSession();
-        assertTrue(session.login("administrator","root"));
+        assertTrue(session.login("administrator", "root"));
         assertTrue(session.isLoggedInAs("administrator"));
     }
 }
